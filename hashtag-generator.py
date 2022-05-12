d267237d03d66b6bd65037702292ad83fdd8abe3
eycanny <101224133+eycanny@users.noreply.github.com>
2022-05-12 11:43:15 -0700

    Update hashtag-generator.py
    - Added start and complete dates
    - Added dividers to easily see separate sections

--- hashtag-generator.py
+++ hashtag-generator.py
@@ -1,3 +1,7 @@
+# Date Started: May 12, 2022
+# Date Completed: May 12, 2022
+
+#--------------------------------------------------------------------#
 # Kata Description
 
 # The marketing team is spending way too much time typing in hashtags.
@@ -9,6 +13,7 @@
 # All words must have their first letter capitalized.
 # If the final result is longer than 140 chars it must return false.
 # If the input or the result is an empty string it must return false.
+#--------------------------------------------------------------------#
 
 def generate_hashtag(s):
     if not s or len(s) > 140:
