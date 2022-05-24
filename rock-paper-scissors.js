bdee1c95cb3c16e2a77e7a55a2dcd6cafc1eb73d
Nancy <101224133+eycanny@users.noreply.github.com>
2022-05-23 20:33:56 -0700

    Add semi-colons

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -15,26 +15,26 @@ const rps = (p1, p2) => {
   
   if (p1 === 'scissors') {
     if (p2 === 'rock') {
-      return 'Player 2 won!'
+      return 'Player 2 won!';
     } else if (p2 === 'paper') {
-      return 'Player 1 won!'
+      return 'Player 1 won!';
     }
   }
 
   
   if (p1 === 'paper') {
     if (p2 === 'scissors') {
-      return 'Player 2 won!'
+      return 'Player 2 won!';
     } else if (p2 === 'rock') {
-      return 'Player 1 won!'
+      return 'Player 1 won!';
     }
   }
   
   if (p1 === 'rock') {
     if (p2 === 'paper') {
-      return 'Player 2 won!'
+      return 'Player 2 won!';
     } else if (p2 === 'scissors') {
-      return 'Player 1 won!'
+      return 'Player 1 won!';
     }
   }
 };
